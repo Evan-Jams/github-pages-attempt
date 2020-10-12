2a90f7bee0ae7c9416de7f93f30ff7bf2294a634
Evan James <thevan777@gmail.com>
2020-10-12 13:22:14 -0400

    added !

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React MAN! Seriously
+          Learn React MAN! Seriously!!!!!
         </a>
       </header>
     </div>
