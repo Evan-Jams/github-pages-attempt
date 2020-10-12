f8fee9f63233c8c56dc04df77036f1c879ab3705
Evan James <thevan777@gmail.com>
2020-10-12 12:59:28 -0400

    Updates

---
+++ precache-manifest.872c15c0cfb67bfb28afae631d82d0e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18c8ac2f43d37baecaa7852f65880cd",
+    "url": "/github-pages-attempt/index.html"
+  },
+  {
+    "revision": "ebeed2b4e442caa18706",
+    "url": "/github-pages-attempt/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3726b365017d83f8d65c",
+    "url": "/github-pages-attempt/static/js/2.642bee43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-attempt/static/js/2.642bee43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebeed2b4e442caa18706",
+    "url": "/github-pages-attempt/static/js/main.b17c9700.chunk.js"
+  },
+  {
+    "revision": "b27fe649dbe2b24657b3",
+    "url": "/github-pages-attempt/static/js/runtime-main.805ae5be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-attempt/static/media/logo.5d5d9eef.svg"
+  }
+]);
